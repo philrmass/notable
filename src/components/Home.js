92be2a5fd2c8a6cf3c89f0c5919eed27426d4b0d
Phil Mass <philrmass@gmail.com>
2024-03-09 17:11:30 -0800

    recovery 3

--- src/components/Home.js
+++ src/components/Home.js
@@ -62,14 +62,12 @@ const defaultNotes = {
 
 export default function Home() {
   const rootName = 'root';
-  /*
-  const [notes, setNotes] = useLocalStorage('nNotes', defaultNotes);
+  // const [notes, setNotes] = useLocalStorage('nNotes', defaultNotes);
   const [parentId, setParentId] = useLocalStorage('nParentId', rootName);
   const [colors, setColors] = useLocalStorage('nColors', defaultColors);
-  */
   const [notes, setNotes] = useState([]);
-  const [parentId, setParentId] = useState('');
-  const [colors, setColors] = useState([]);
+  // const [parentId, setParentId] = useState('');
+  // const [colors, setColors] = useState([]);
 
   const [message, setMessage] = useState('');
   const [topMenuShown, setTopMenuShown] = useState(false);
